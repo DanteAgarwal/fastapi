2de0ce5cee69afed814a5e26a2b352a75503c5df
Akshat k Goyal <46156730+DanteAgarwal@users.noreply.github.com>
2025-06-08 16:31:08 +0000

    just litttle playinmg around

--- tutorial_fastapi/main.py
+++ tutorial_fastapi/main.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get("/")
 async def read_root():
     return {"message": "Hello, World!"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id:int):
+    return {"data":item_id} #fetch item by id=id
+
+@app.get("/items/{item_id}/name")
+async def read_item_name(item_id:int):
+    return {"data":{item_id:{"item_name"*3:"item_name"}}} #fetch item by id=id and return item_name
